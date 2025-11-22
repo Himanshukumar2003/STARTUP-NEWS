f70f58e056bbcbe6276c38b1c09409ede770faef
unknown <devliyalhimanshu@gmsil.com>
2025-11-22 18:48:27 +0530

    first commit

--- js/type.js
+++ js/type.js
@@ -13,17 +13,29 @@ var typed = new Typed("#typeTextTwo", {
   showCursor: false, // hide the blinking cursor
 });
 
-var typed = new Typed("#typeTextThree", {
-  strings: [
-    "#1 Indian Startup and Funding News Site  ^3000",
-    "India’s Premier Startup News Network ^3000",
-    "#1 Destination for Indian Startup News ^3000",
-  ],
-  typeSpeed: 50, // typing speed
-  backSpeed: 100, // backspacing speed (if loop)
-  startDelay: 300,
-  backDelay: 3000, // 3 sec delay before next
+var textWrapper = document.querySelector(".ml10 .letters");
 
-  loop: true, // change to true if you want continuous typing
-  showCursor: false, // hide the blinking cursor
+document.querySelectorAll(".ml10 .letters").forEach((textWrapper, index) => {
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+
+  anime
+    .timeline({ loop: false })
+    .add({
+      targets: textWrapper.querySelectorAll(".letter"),
+      rotateY: [-90, 0],
+      opacity: [0, 1],
+      duration: 100,
+      delay: (el, i) => 70 * i,
+    })
+    .add({
+      targets: textWrapper,
+      opacity: 1,
+      loop: false,
+      duration: 3000,
+      easing: "easeOutExpo",
+      delay: 1000,
+    });
 });
