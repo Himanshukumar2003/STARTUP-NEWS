11e0764ca17f380b09d21d4ce40383388431597f
unknown <devliyalhimanshu@gmsil.com>
2025-11-22 16:27:50 +0530

    first commit

--- js/type.js
+++ js/type.js
@@ -1,17 +1,3 @@
-var typed = new Typed("#typeText", {
-  strings: [
-    "1 Million+ Monthly Visitors Trust Us. We’re Google News Approved! Share Your Startup Story 👉   contact@startupnewsindia.in ^3000",
-    "#1 Destination for Indian Startup News ^3000",
-  ],
-  typeSpeed: 50, // typing speed
-  backSpeed: 100, // backspacing speed (if loop)
-  startDelay: 300,
-  backDelay: 3000, // 3 sec delay before next
-
-  loop: true, // change to true if you want continuous typing
-  showCursor: false, // hide the blinking cursor
-});
-
 var typed = new Typed("#typeTextTwo", {
   strings: [
     "India’s Best Startup News Website ^3000",
@@ -31,6 +17,7 @@ var typed = new Typed("#typeTextThree", {
   strings: [
     "#1 Indian Startup and Funding News Site  ^3000",
     "India’s Premier Startup News Network ^3000",
+    "#1 Destination for Indian Startup News ^3000",
   ],
   typeSpeed: 50, // typing speed
   backSpeed: 100, // backspacing speed (if loop)
